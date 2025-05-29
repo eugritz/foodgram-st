7e27429c298769f1daefe1b75c1aa7a11cbc59df
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-05-29 20:44:38 +0600

    Добавить поле avatar в модель User

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True)
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        null=True,  
+        default=None,
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
