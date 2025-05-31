e62b56f2f67cee62fa498d57b03a841bd0ca8139
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-05-31 13:23:29 +0600

    Исправить поля поиска для моделей в панели администратора

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -26,42 +26,52 @@ class BaseModelAdmin(admin.ModelAdmin):
 
 class IngredientAdmin(ImportExportModelAdmin):
     list_display = ('pk', 'name', 'measurement_unit')
-    search_fields = ('user',)
+    search_fields = ('name',)
     empty_value_display = '-пусто-'
     resource_classes = [IngredientResource]
 
 
 class SubscriptionAdmin(BaseModelAdmin):
     list_display = ('pk', 'user', 'subscribed_to')
-    search_fields = ('user', 'subscribed_to')
+    search_fields = ('user__email', 'subscribed_to__email')
+
+
+@admin.display(description='Author')
+def author(obj):
+    return f'{obj.author.email} ({obj.author.first_name} {obj.author.last_name})'
 
 
 class RecipeAdmin(BaseModelAdmin):
     list_display = (
         'pk',
-        'author',
+        author,
         'name',
         'image',
         'text',
         'cooking_time',
         'created_at',
     )
-    search_fields = ('author', 'name')
+    search_fields = (
+        'author__email',
+        'author__first_name',
+        'author__last_name',
+        'name'
+    )
 
 
 class RecipeIngredientAdmin(BaseModelAdmin):
     list_display = ('pk', 'recipe', 'ingredient', 'amount')
-    search_fields = ('recipe', 'ingredient')
+    search_fields = ('recipe__name', 'ingredient__name')
 
 
 class FavoriteAdmin(BaseModelAdmin):
     list_display = ('pk', 'user', 'recipe')
-    search_fields = ('user', 'recipe')
+    search_fields = ('user__email', 'recipe__name')
 
 
 class ShoppingCartAdmin(BaseModelAdmin):
     list_display = ('pk', 'user', 'recipe')
-    search_fields = ('user', 'recipe')
+    search_fields = ('user__email', 'recipe__name')
 
 
 admin.site.register(Favorite, FavoriteAdmin)
